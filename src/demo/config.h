1e8fa99bda6683e9d975877599d434b4c47797a5
liautumn <1060590974@qq.com>
2024-06-21 16:10:19 +0800

    Update config.h

--- src/demo/config.h
+++ src/demo/config.h
@@ -9,36 +9,6 @@ class Config {
 public:
     const string MODEL = R"(D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\model\engine\yolov8s.engine)";
     const string TEST_IMG = R"(D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\images\car.jpg)";
-
-    const char *cocolabels[80] = {
-            "person", "bicycle", "car",
-            "motorcycle", "airplane", "bus",
-            "train", "truck", "boat",
-            "traffic light", "fire hydrant", "stop sign",
-            "parking meter", "bench", "bird",
-            "cat", "dog", "horse",
-            "sheep", "cow", "elephant",
-            "bear", "zebra", "giraffe",
-            "backpack", "umbrella", "handbag",
-            "tie", "suitcase", "frisbee",
-            "skis", "snowboard", "sports ball",
-            "kite", "baseball bat", "baseball glove",
-            "skateboard", "surfboard", "tennis racket",
-            "bottle", "wine glass", "cup",
-            "fork", "knife", "spoon",
-            "bowl", "banana", "apple",
-            "sandwich", "orange", "broccoli",
-            "carrot", "hot dog", "pizza",
-            "donut", "cake", "chair",
-            "couch", "potted plant", "bed",
-            "dining table", "toilet", "tv",
-            "laptop", "mouse", "remote",
-            "keyboard", "cell phone", "microwave",
-            "oven", "toaster", "sink",
-            "refrigerator", "book", "clock",
-            "vase", "scissors", "teddy bear",
-            "hair drier", "toothbrush"
-    };
 };
 
 #endif //YOLO_CONFIG_H
